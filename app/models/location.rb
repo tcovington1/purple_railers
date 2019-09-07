ce24f8a963c9beb1c02e4dc2204221bde070a835
Matt Uyehara <laieboy@gmail.com>
2019-09-07 16:36:46 -0600

    Fixed all Trip CRUD

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ApplicationRecord
-  belongs_to :trip
-  has_one :address
+  belongs_to :trip, dependent: :destroy
+  has_one :address, dependent: :destroy
   validates :name, :days, presence: true
 end
